a6f99909d824c0892ecf0f87f30fd00f1d18f053
Tech-A20 <Tech-A20@Tech-A20s-MacBook-Air.local>
2017-10-30 14:47:42 +0000

    if else statements

---
+++ sparta-js-lab/data-type-starter-code/sparta-js-controlflow/main.js
@@ -0,0 +1,11 @@
+var moneyInPocket = prompt('How much do you have to spend?');
+
+if (moneyInPocket > 10){
+  console.log("I can buy a drink with my lunch");
+}  else if (moneyInPocket > 5) {
+  console.log("I can buy just some crisps");
+}else {
+  {
+  console.log("I cant buy anything");
+  }
+}
